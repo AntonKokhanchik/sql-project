c64865bda340c24cf20489cfd9aeab32e52408cd
Anton <kokhanchik.tversu@gmail.com>
2017-04-06 14:59:26 +0300

    beautifier

--- parser/app.rb
+++ parser/app.rb
@@ -27,119 +27,115 @@ DB = Sequel.connect(
 
 @name_index = 4 # индекс id в ссыдке
 @genre_inex = 5
-@link = "https://www.kinopoisk.ru"
+@link = 'https://www.kinopoisk.ru'
 
-def nameParser href, db_name
+def nameParser(href, db_name)
   sleep rand(10..30)
   page = Nokogiri::HTML(open(href))
   id = href.split('/')[@name_index]
 
-  name = page.css("#headerPeople h1").text.split(' ')[0]
+  name = page.css('#headerPeople h1').text.split(' ')[0]
   puts name
-  surname = page.css("#headerPeople h1").text.split(' ')[1]
+  surname = page.css('#headerPeople h1').text.split(' ')[1]
 
   year =
-  if page.css("#infoTable .info tr:nth-child(2) .type").text == "дата рождения"
-    if page.css("#infoTable .info tr:nth-child(2) a:nth-child(2)") != nil
-      page.css("#infoTable .info tr:nth-child(2) a:nth-child(2)").text
+    if page.css('#infoTable .info tr:nth-child(2) .type').text == 'дата рождения'
+      if page.css('#infoTable .info tr:nth-child(2) a:nth-child(2)') != nil
+        page.css('#infoTable .info tr:nth-child(2) a:nth-child(2)').text
+      else
+        '0'
+      end
+    elsif page.css('#infoTable .info tr:nth-child(3) .type').text == 'дата рождения'
+      if page.css('#infoTable .info tr:nth-child(3) a:nth-child(2)') != nil
+        page.css('#infoTable .info tr:nth-child(3) a:nth-child(2)').text
+      else
+        '0'
+      end
     else
-      "0"
+      '0'
     end
-  elsif page.css("#infoTable .info tr:nth-child(3) .type").text == "дата рождения"
-    if page.css("#infoTable .info tr:nth-child(3) a:nth-child(2)") != nil
-      page.css("#infoTable .info tr:nth-child(3) a:nth-child(2)").text
-    else
-      "0"
-    end
-  else
-    "0"
-  end
-  unless year =~ /\A\d+\Z/
-    year = 0
-  end
+  year = 0 unless year =~ /\A\d+\Z/
 
   temp =
-  if page.css("#infoTable .info tr:nth-child(3) .type").text == "место рождения"
-    if page.css("#infoTable .info tr:nth-child(3) span a:nth-child(3)")[0] != nil
-      page.css("#infoTable .info tr:nth-child(3) span a:nth-child(3)")[0].text
-    elsif page.css("#infoTable .info tr:nth-child(3) span a:nth-child(2)")[0] != nil
-      page.css("#infoTable .info tr:nth-child(3) span a:nth-child(2)")[0].text
+    if page.css('#infoTable .info tr:nth-child(3) .type').text == 'место рождения'
+      if page.css('#infoTable .info tr:nth-child(3) span a:nth-child(3)')[0] != nil
+        page.css('#infoTable .info tr:nth-child(3) span a:nth-child(3)')[0].text
+      elsif page.css('#infoTable .info tr:nth-child(3) span a:nth-child(2)')[0] != nil
+        page.css('#infoTable .info tr:nth-child(3) span a:nth-child(2)')[0].text
+      else
+        'неизвестно'
+      end
+    elsif page.css('#infoTable .info tr:nth-child(4) .type').text == 'место рождения'
+      if page.css('#infoTable .info tr:nth-child(4) span a:nth-child(3)')[0] != nil
+        page.css('#infoTable .info tr:nth-child(4) span a:nth-child(3)')[0].text
+      elsif page.css('#infoTable .info tr:nth-child(4) span a:nth-child(2)')[0] != nil
+        page.css('#infoTable .info tr:nth-child(4) span a:nth-child(2)')[0].text
+      else
+        'неизвестно'
+      end
     else
-      "неизвестно"
+      'неизвестно'
     end
-  elsif page.css("#infoTable .info tr:nth-child(4) .type").text == "место рождения"
-    if page.css("#infoTable .info tr:nth-child(4) span a:nth-child(3)")[0] != nil
-      page.css("#infoTable .info tr:nth-child(4) span a:nth-child(3)")[0].text
-    elsif page.css("#infoTable .info tr:nth-child(4) span a:nth-child(2)")[0] != nil
-      page.css("#infoTable .info tr:nth-child(4) span a:nth-child(2)")[0].text
-    else
-      "неизвестно"
-    end
-  else
-    "неизвестно"
-  end
-  if @countries[country_name: temp] == nil then
+  if @countries[country_name: temp].nil?
     DB.transaction do
-      @countries.insert(:country_name => temp)
+      @countries.insert(country_name: temp)
     end
   end
   country = @countries.where(country_name: temp).first[:country_id]
 
   DB.transaction do
     case db_name
-    when "directors"
+    when 'directors'
       @directors.insert(
-      :director_id => id,
-      :director_name => name,
-      :director_surname => surname,
-      :birth_year => year,
-      :country_id => country
+        director_id: id,
+        director_name: name,
+        director_surname: surname,
+        birth_year: year,
+        country_id: country
       )
-    when "producers"
+    when 'producers'
       @producers.insert(
-      :producer_id => id,
-      :producer_name => name,
-      :producer_surname => surname,
-      :birth_year => year,
-      :country_id => country
+        producer_id: id,
+        producer_name: name,
+        producer_surname: surname,
+        birth_year: year,
+        country_id: country
       )
-    when "screenwriters"
+    when 'screenwriters'
       @screenwriters.insert(
-      :screenwriter_id => id,
-      :screenwriter_name => name,
-      :screenwriter_surname => surname,
-      :birth_year => year,
-      :country_id => country
+        screenwriter_id: id,
+        screenwriter_name: name,
+        screenwriter_surname: surname,
+        birth_year: year,
+        country_id: country
       )
-    when "actors"
+    when 'actors'
       @actors.insert(
-      :actor_id => id,
-      :actor_name => name,
-      :actor_surname => surname,
-      :birth_year => year,
-      :country_id => country
+        actor_id: id,
+        actor_name: name,
+        actor_surname: surname,
+        birth_year: year,
+        country_id: country
       )
     end
   end
 end
 
-def actorsParser href, id
+def actorsParser(href, id)
   page = Nokogiri::HTML(open(href))
 
   5.times do |i|
-    temp = @link + page.css("#block_left > div > div.dub > div.actorInfo > div.info > div.name > a")[i+1]['href']
+    temp = @link + page.css('#block_left > div > div.dub > div.actorInfo > div.info > div.name > a')[i + 1]['href']
     actor = temp.split('/')[@name_index]
-    role = page.css("#block_left > div > div.dub > div.actorInfo > div.info > div.role")[i+1].text
+    role = page.css('#block_left > div > div.dub > div.actorInfo > div.info > div.role')[i + 1].text
     puts role
 
-    if @actors[actor_id: actor] == nil then
-      nameParser(temp, "actors")
-    end
+    nameParser(temp, 'actors') if @actors[actor_id: actor].nil?
     DB.transaction do
       @actors_films.insert(
-      :film_id => id,
-      :actor_id => actor,
-      :character_name => role
+        film_id: id,
+        actor_id: actor,
+        character_name: role
       )
     end
   end
@@ -151,52 +147,46 @@ end
   page_link = @link + "/film/#{id}"
   page = Nokogiri::HTML(open(page_link))
 
-  name = page.css("#headerFilm h1").text
+  name = page.css('#headerFilm h1').text
   puts name
-  year = page.css("#infoTable .info tr:nth-child(1) a").text
+  year = page.css('#infoTable .info tr:nth-child(1) a').text
   puts year
-  rating = page.css("#block_rating div div a .rating_ball").text
-  unless rating =~ /\A\d+\Z/
-    rating = 0
-  end
+  rating = page.css('#block_rating div div a .rating_ball').text
+  rating = 0 unless rating =~ /\A\d+\Z/
   puts rating
-  temp = @link + page.css("#infoTable .info tr:nth-child(4) a")[0]['href']
+  temp = @link + page.css('#infoTable .info tr:nth-child(4) a')[0]['href']
   director = temp.split('/')[@name_index]
-  if @directors[director_id: director] == nil then
-    nameParser(temp, "directors")
-  end
+  nameParser(temp, 'directors') if @directors[director_id: director].nil?
   puts director
 
-  temp = @link + page.css("#infoTable .info tr:nth-child(6) a:first-child")[0]['href']
+  temp = @link + page.css('#infoTable .info tr:nth-child(6) a:first-child')[0]['href']
   producer = temp.split('/')[@name_index]
-  if @producers[producer_id: producer] == nil then
-    nameParser(temp, "producers")
-  end
+  nameParser(temp, 'producers') if @producers[producer_id: producer].nil?
   puts producer
 
-  temp = @link + page.css("#infoTable .info tr:nth-child(5) a:first-child")[0]['href']
+  temp = @link + page.css('#infoTable .info tr:nth-child(5) a:first-child')[0]['href']
   screenwriter = temp.split('/')[@name_index]
-  if @screenwriters[screenwriter_id: screenwriter] == nil then
-    nameParser(temp, "screenwriters")
+  if @screenwriters[screenwriter_id: screenwriter].nil?
+    nameParser(temp, 'screenwriters')
   end
   puts screenwriter
 
-  temp = page.css("#infoTable .info tr:nth-child(2) a:first-child")[0].text
-  if @countries[country_name: temp] == nil then
+  temp = page.css('#infoTable .info tr:nth-child(2) a:first-child')[0].text
+  if @countries[country_name: temp].nil?
     DB.transaction do
-      @countries.insert(:country_name => temp)
+      @countries.insert(country_name: temp)
     end
   end
   country = @countries.where(country_name: temp).first[:country_id]
   puts country
 
-  temp = page.css("#infoTable .info tr:nth-child(11) a:first-child")[0]
-  genre =  (@link + temp['href']).split('/')[@genre_inex]
-  if @genres[genre_id: genre] == nil then
+  temp = page.css('#infoTable .info tr:nth-child(11) a:first-child')[0]
+  genre = (@link + temp['href']).split('/')[@genre_inex]
+  if @genres[genre_id: genre].nil?
     DB.transaction do
       @genres.insert(
-      :genre_id => genre,
-      :genre_name => temp.text
+        genre_id: genre,
+        genre_name: temp.text
       )
     end
   end
@@ -204,21 +194,20 @@ end
 
   DB.transaction do
     @films.insert(
-    :film_id => id,
-    :film_name => name,
-    :release_year => year,
-    :rating => rating,
-    :director_id => director,
-    :producer_id => producer,
-    :screenwriter_id => screenwriter,
-    :country_id => country,
-    :genre_id => genre
+      film_id: id,
+      film_name: name,
+      release_year: year,
+      rating: rating,
+      director_id: director,
+      producer_id: producer,
+      screenwriter_id: screenwriter,
+      country_id: country,
+      genre_id: genre
     )
   end
 
-  actorsParser(page_link + "/cast", id)
-
   sleep(rand(30..60))
+  actorsParser(page_link + '/cast', id)
 
   sleep(rand((30..60)))
 end

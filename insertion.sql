e49b2aebb76a72a351d5356bd836636b37decb40
Anton Kokhanchik <kokhanchik.tversu@gmail.com>
2017-04-07 07:44:32 +0300

    insertion template

---
+++ insertion.sql
@@ -0,0 +1,26 @@
+insert into cities values
+();
+
+insert into  directors 
+(director_name, director_surname, birth_year, country_id)
+values
+();
+
+insert into producers 
+(producer_name, producer_surname, birth_year, country_id)
+values
+();
+
+insert into screenwriters 
+(screenwriter_name, screenwriter_surname, birth_year, country_id)
+values
+();
+
+insert into genres (genre_name) values
+();
+
+insert into films (film_name, release_year, rating, director_id, producer_id, screenwriter_id, country_id, genre_id) values
+();
+
+insert into rewardings_Oscar (rewarding_year, actor_id, film_id
+

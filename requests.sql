01517082fb49f3945ca7ff67422d42018ecbf78e
Mariya <mmamonova95@gmail.com>
2017-04-18 12:33:32 +0300

    still have 3 requests

--- requests.sql
+++ requests.sql
@@ -1,13 +1,7 @@
-# TODO: 30.2+
-# TODO: 34 - переделать само задание (возникает повтор с запросм 22)
 # TODO: 36, 39+
 #		+
 #данные: 
-#	больше рецензий о ВКВК
-#	больше актеров из Германии, игравших в фильмах, снятых в США
-#
 # 		больше английского фэнтези (для запроса35)
-
 #		добавить автора статей, которые всегда ставят оценки ниже рейтинга фильма (несколько его статей с низким рейтингом)
 #		добавить актеров, которые являются режиссерами или продюсерами в любых фильмах (т.е., не обязательно в том же, где они играют)
 
@@ -132,11 +126,13 @@ select nomination, rewarding_year, film_name from rewardings_Oscar as t1 natural
 		exists(select * from actors where concat(actor_name, " ", actor_surname) = "Мэрил Стрип" and actors.actor_id = actors_films.actor_id));
 
 # 21. Вывести всех актеров в порядке возрастания успешности (имеется в виду количество полученных наград)
-select concat(actor_name, " ", actor_surname) as актёр, count((select actor_id from rewardings_Oscar where rewardings_Oscar.actor_id = t1.actor_id)) as количество_наград 
-	from actors as t1 group by actor_id order by количество_наград;
+select concat(actor_name, " ", actor_surname) as актёр, count(nomination) as количество_наград 
+	from actors as t1 natural join rewardings_oscar group by actor_id order by количество_наград;
     
-# 22. Вывести 5 наиболее востребованных актеров за 2000-2005 годы (тех, кто снимался в большем количестве фильмов)
-select concat(actor_name, " ", actor_surname) as актёр, count(film_id) as количество_фильмов from actors natural join actors_films group by actor_id order by количество_фильмов desc limit 5;
+# 22. Вывести 4 наиболее востребованных актеров за 2000-2005 годы (тех, кто снимался в большем количестве фильмов)
+select concat(actor_name, " ", actor_surname) as актёр, count(film_id) as количество_фильмов from actors natural join actors_films as t1
+where exists(select * from films as t2 where release_year between 2000 and 2005 and t2.film_id = t1.film_id)
+group by actor_id order by количество_фильмов desc limit 4;
 
 # 23. Вывести список фильмов, снятых на территории Великобритании, имеющих рейтинг больше 8
 select film_name from films where rating > 8 and exists(select * from countries where country_name = "Великобритания" and countries.country_id = films.country_id);
@@ -190,3 +186,41 @@ select country_name from countries as t1 where exists(
 select distinct country_name from countries natural join films as t2 where exists(
 	select * from rewardings_oscar as t3 where t3.film_id = t2.film_id
 );
+
+# 33. Вывести все рецензии, оценка фильма от автора у которых выше рейтинга самого фильма
+select * from reviews as t1 where author_mark > (
+	select rating from films as t2 where t1.film_id = t2.film_id
+);
+
+# 34. Вывести режиссёров, которые писали сценарий к своему фильму
+select concat(director_name, " ", director_surname) as режиссер from directors as t1 where 
+	exists(select * from screenwriters as t2 where concat(t1.director_name, " ", t1.director_surname) = concat(t2.screenwriter_name, " ", t2.screenwriter_surname))
+    and exists(select * from films as t3 where director_id = screenwriter_id and t3.director_id = t1.director_id);
+
+# 35. Вывести все фильмы, которые снимались в той же стране, что и Гарри Поттер, и они того же жанра
+select distinct if(country_id = (select country_id from films where film_name = "Гарри Поттер и Тайная комната") 
+		  and genre_id = (select genre_id from films where film_name = "Гарри Поттер и Тайная комната"), film_name, null) as фильмы from films;
+
+# 36. Вывести для каждого актера из списка его фильмов тот, у которого наибольший рейтинг
+#select	(select concat(actor_name, " ", actor_surname) as актёр from actors as t2 where t2.actor_id = t1.actor_id),
+#		(select film_name from films as t3 where t3.film_id = t1.film_id and rating = max(rating))
+#from actors_films as t1;
+
+# 37. Вывести авторов статей, которые всегда ставят оценки ниже рейтинга фильма
+select distinct author_name from reviews as t1 where not exists(
+	select author_mark from reviews as t3 where author_mark > (
+		select rating from films as t2 where t3.film_id = t2.film_id
+	) and t1.author_name = t3.author_name
+);
+
+# 38. Вывести все страны, в которых снимались фильмы в порядке убывания количества снятых в них фильмов
+select country_name, (select count(film_id) from films as t3 where t1.country_id = t3.country_id) as количество_фильмов from countries as t1 where exists(select * from films as t2 where t1.country_id = t2.country_id) order by количество_фильмов desc;
+
+select country_name, count(film_id) as количество_фильмов from countries as t1 natural join films where if(country_id in (select country_id from films as t2 where t1.country_id = t2.country_id), true, false) group by country_id order by количество_фильмов desc;
+
+# 39. Вывести режиссеров в порядке убывания количества наград, полученных актерами за съемки в их фильмах
+
+# 40. Вывести всех актеров, которые являются ещё и режиссерами или продюсерами (возможно в других фильмах)
+#select concat(actor_name, " ", actor_surname) as актёр from actors as t1 where if(
+#	exists(select * from directors as t2 where concat(actor_name, " ", actor_surname) = concat(director_name, " ", director_surname)), true,false
+#);
